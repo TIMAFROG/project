dca2fa9f049be4a3342431aef477cf1fb7774014
TIMAFROG <151742842+TIMAFROG@users.noreply.github.com>
2024-03-09 22:22:08 +0300

    Create data.py
    add db

---
+++ data/data.py
@@ -0,0 +1,18 @@
+from sqlalchemy import orm
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Data(SqlAlchemyBase):
+    __tablename__ = 'data'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+
+    password = sqlalchemy.Column(sqlalchemy.String, unique=True)
+
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+
+    telephone = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+
+    name = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey("users.name"))
+    name_user = orm.relationship('User')
