f365dded847f903485e554a34c246f2b0f759113
TIMAFROG <151742842+TIMAFROG@users.noreply.github.com>
2024-03-09 22:23:01 +0300

    Create donations.py
    + db

---
+++ data/donations.py
@@ -0,0 +1,16 @@
+from sqlalchemy import orm
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class Donations(SqlAlchemyBase):
+    __tablename__ = 'donations'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+
+    info = sqlalchemy.Column(sqlalchemy.String, unique=True)
+
+    money = sqlalchemy.Column(sqlalchemy.Integer, index=True)
+
+    donate = sqlalchemy.Column(sqlalchemy.String, nullable=False, default='Игрок')
+    d = orm.relationship("User", back_populates='donate_donations')
