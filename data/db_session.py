23524fcba5329fc1d43a9a1e161403faa7387318
TIMAFROG <151742842+TIMAFROG@users.noreply.github.com>
2024-04-21 15:04:37 +0300

    Update db_session.py

--- data/db_session.py
+++ data/db_session.py
@@ -1,9 +1,9 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
+import sqlalchemy.ext.declarative as dec
 
-
-SqlAlchemyBase = orm.declarative_base()
+SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
 
