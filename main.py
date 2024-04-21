21d8dd771e7043fef181c151440127ce6d5c0999
TIMAFROG <151742842+TIMAFROG@users.noreply.github.com>
2024-04-21 15:03:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from data.users import User
+from data.donations import Donations
 from data import db_session
 
 app = Flask(__name__)
@@ -6,8 +8,10 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 def main():
+
     db_session.global_init("db/db_proekt.db")
-    app.run()
+
+    # app.run()
 
 
 if __name__ == '__main__':
